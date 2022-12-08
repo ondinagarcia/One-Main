d1c4c64029935a5a0f46a285664efe9520940b5e
Your Name <you@example.com>
2022-12-08 12:48:10 -0600

    agregandole estilos al index

--- js/main.js
+++ js/main.js
@@ -1,13 +1,49 @@
+// Constante para el body
+const body = document.querySelector('body');
 
-// llamar nuestras constantes principale
+// Llamar nuestras constantes principales
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const seccionPrincipal = document.getElementById('contenido-1');
+
+// Inputs de la forma
+const sentimiento = document.getElementById('sentimiento');
+
+// Crear un escuchador de eventos para la forma
+form.addEventListener('submit', (e) => {
+  // Evitar accion por defecto de la forma
+  e.preventDefault();
+
+});
+
+// Crear un evento global 
+main.addEventListener('click', (e) => {
+  if(e.target.id === 'feliz') {
+    // Agregar clases a body
+    body.removeAttribute('class');
+    body.classList.add('feliz');
+
+    // Agregar la clase a la seccion
+    seccionPrincipal.classList.add('feliz-s');
+  }
+
+  if(e.target.id === 'triste') {
+    body.removeAttribute('class');
+    body.classList.add('triste');
+  }
+
+  if(e.target.id === 'tranquilo') {
+    body.removeAttribute('class');
+    body.classList.add('tranquilo');
+  }
+
+  if(e.target.id === 'enojado') {
+    body.removeAttribute('class');
+    body.classList.add('enojado');
+
+    // Agregar la clase a la seccion
+    seccionPrincipal.classList.add('enojado-s');
+  }
+});
 
 
-// imput de la forma
-
-
-
-// evitar accion por defecto de la forma
-
-
-
-//constante para el body
